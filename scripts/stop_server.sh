c4e790eefdd53543ed5291a46514e7da83a1edd5
vunt-3278 <nguyen.the.vu@sun-asterisk.com>
2024-06-24 13:23:11 +0700

    modify

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo systemctl stop nginx
+ls -la
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello
+          Hello world!!!
         </a>
       </header>
     </div>
